2f843a4fc4ab1575b219dcf5b7cc7390413174c8
Ricardo <ricardo@Ricardos-MacBook-Pro.local>
2025-07-15 19:12:37 -0400

    Use rate-limiter module in /shorten and /

--- main.go
+++ main.go
@@ -5,20 +5,22 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/RicardoTlatelpa/go-rate-limiter/middleware"
 	gen "github.com/RicardoTlatelpa/uniqueidgen"
 	"github.com/redis/go-redis/v9"
 )
 var (
 	redisClient *redis.Client
 	ctx = context.Background()
-
+	
 	idGen *gen.Gen	
 	baseURL = "http://localhost:8080/"
 )
 
 func main() {
-	var err error
-
+	var err error	
+	shorten := middleware.RedisRateLimitMiddleware(5,1.0, http.HandlerFunc(handleShorten))
+	getURL := middleware.RedisRateLimitMiddleware(5,1.0, http.HandlerFunc(handleRedirect))
 	redisClient = redis.NewClient(&redis.Options{
 		Addr:		"localhost:6379",
 		Password:	"",
@@ -34,8 +36,8 @@ func main() {
 		log.Fatalf("failed to create generator: %v", err)
 	}
 
-	http.HandleFunc("/shorten", handleShorten)
-	http.HandleFunc("/", handleRedirect)
+	http.Handle("/shorten", shorten)
+	http.Handle("/", getURL)
 
 	log.Println("server started at :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
